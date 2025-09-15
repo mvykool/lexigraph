7452612cb8588984b52780e4106187236139cda8
Maicol Hernandez <maykolandres4@gmail.com>
2025-09-14 21:04:56 -0400

    added serde and name query as query param

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,15 @@
-#![allow(unused)] // temporary
-use axum::response::Html;
+#![allow(unused)]
+use axum::extract::Query;
+// temporary
+use axum::response::{Html, IntoResponse};
 use axum::{routing::get, Router};
+use serde::Deserialize;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("hello <strong>world!!</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
+
     // region --start server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     print!("->> LISTENING ON {addr}\n");
@@ -16,4 +17,16 @@ async fn main() {
         .serve(routes_hello.into_make_service())
         .await
         .unwrap()
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>"))
 }
