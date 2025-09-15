5a2ff4cf4fdde4610db9579587937ea841f7e0e8
Maicol Hernandez <maykolandres4@gmail.com>
2025-09-14 20:48:35 -0400

    update, create test and return html

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,19 @@
+#![allow(unused)] // temporary
+use axum::response::Html;
 use axum::{routing::get, Router};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "hellow world" }));
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap()
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("hello <strong>world!!</strong>") }),
+    );
+    // region --start server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    print!("->> LISTENING ON {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap()
 }
