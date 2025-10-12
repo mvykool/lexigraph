014f2bb16b1aabd41b83da183f973288c1126231
Maicol Hernandez <maykolandres4@gmail.com>
2025-10-12 05:38:08 -0400

    added more types

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,5 @@
 #![allow(unused)]
 use axum::extract::Query;
-// temporary
 use axum::response::{Html, IntoResponse};
 use axum::{routing::get, Router};
 use serde::Deserialize;
@@ -8,10 +7,10 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_hello: Router = Router::new().route("/hello", get(handler_hello));
 
     // region --start server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 8080));
     print!("->> LISTENING ON {addr}\n");
     axum::Server::bind(&addr)
         .serve(routes_hello.into_make_service())
@@ -27,6 +26,6 @@ struct HelloParams {
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
 
-    let name = params.name.as_deref().unwrap_or("World!");
+    let name: &str = params.name.as_deref().unwrap_or("World!");
     Html(format!("Hello <strong>{name}</strong>"))
 }
